56f0f53d7add5a388ff77552bbb9d7dbaa41a7c7
lbortman <limor.bortman@alcatel-lucent.com>
2015-02-22 13:50:56 +0200

    adding : 1) delete stack 2) update stack 3) list stack resources 4) get stack resource 5)get Stack Resource Metadata (cherry picked from commit 9a83017)

--- apis/openstack-heat/src/test/java/org/jclouds/openstack/heat/v1/features/StackApiLiveTest.java
+++ apis/openstack-heat/src/test/java/org/jclouds/openstack/heat/v1/features/StackApiLiveTest.java
@@ -16,14 +16,21 @@
 */
 package org.jclouds.openstack.heat.v1.features;
 
+import com.google.common.base.Predicate;
 import com.google.common.base.Throwables;
+import com.google.common.collect.FluentIterable;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Sets;
 import org.jclouds.openstack.heat.v1.domain.Stack;
+import org.jclouds.openstack.heat.v1.domain.StackResource;
 import org.jclouds.openstack.heat.v1.internal.BaseHeatApiLiveTest;
 import org.jclouds.openstack.heat.v1.options.CreateStackOptions;
+import org.jclouds.openstack.heat.v1.options.UpdateOptions;
 import org.jclouds.util.Strings2;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -31,7 +38,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.jclouds.util.Predicates2.retry;
 
 /**
  * // * Tests parsing and Guice wiring of StackApi
@@ -91,6 +100,20 @@ public class StackApiLiveTest extends BaseHeatApiLiveTest {
       }
    }
 
+   public void testDeleteStack() {
+      for (String region : api.getConfiguredRegions()) {
+         StackApi stackApi = api.getStackApi(region);
+         CreateStackOptions createStackOptions = CreateStackOptions.Builder.templateUrl(TEMPLATE_URL);
+         String stackName = getName();
+         Stack stack = stackApi.create(stackName, createStackOptions);
+         assertThat(stack).isNotNull();
+         assertThat(stack.getId()).isNotEmpty();
+         assertThat(stackApi.delete(stackName, stack.getId())).isTrue();
+         Stack stackAfterDelete = stackApi.get(stackName, stack.getId());
+         assertThat(stackAfterDelete).isNotNull();
+      }
+   }
+
    public void testCreateWithDisableRollback() {
       for (String region : api.getConfiguredRegions()) {
          StackApi stackApi = api.getStackApi(region);
@@ -115,6 +138,40 @@ public class StackApiLiveTest extends BaseHeatApiLiveTest {
          Stack stack = stackApi.create(getName(), createStackOptions);
          assertThat(stack).isNotNull();
          assertThat(stack.getId()).isNotEmpty();
+      }
+   }
+
+   public void testUpdateStack() throws ParseException {
+      for (String region : api.getConfiguredRegions()) {
+         final StackApi stackApi = api.getStackApi(region);
+         JSONParser parser = new JSONParser();
+         Object obj = parser.parse(stringFromResource("/simple_stack.json"));
+
+         JSONObject jsonObject = (JSONObject) obj;
+
+         CreateStackOptions createStackOptions = CreateStackOptions.Builder.template(String.valueOf(jsonObject.get("template")));
+         String stackName = getName();
+         Stack stack = stackApi.create(stackName, createStackOptions);
+         assertThat(stack).isNotNull();
+         String stackId = stack.getId();
+         assertThat(stackId).isNotEmpty();
+
+         boolean success = retry(new Predicate<String>() {
+            public boolean apply(String stackId ) {
+               return stackApi.get(stackId).getStatus() == Stack.Status.CREATE_COMPLETE;
+            }
+
+         }, 60, 1, SECONDS).apply(stackId);
+
+         if (!success) {
+            Assert.fail("Stack didn't get to status CREATE_COMPLETE in 20m.");
+         }
+
+         UpdateOptions updateOptions = UpdateOptions.Builder.templateUrl(TEMPLATE_URL);
+         assertThat(stackApi.update(stackName, stack.getId(), updateOptions)).isTrue();
+         Stack stackAfterUpdate = stackApi.get(stackName, stack.getId());
+         assertThat(stackAfterUpdate.getStatus()).isEqualTo(Stack.Status.UPDATE_IN_PROGRESS);
+
       }
    }
 
@@ -164,6 +221,68 @@ public class StackApiLiveTest extends BaseHeatApiLiveTest {
          Stack stack = stackApi.create(getName(), createStackOptions);
          assertThat(stack).isNotNull();
          assertThat(stack.getId()).isNotEmpty();
+      }
+   }
+
+   public void testListAndGetStackResources() throws ParseException {
+      for (String region : api.getConfiguredRegions()) {
+         StackApi stackApi = api.getStackApi(region);
+         JSONParser parser = new JSONParser();
+         Object obj = parser.parse(stringFromResource("/stack_with_parameters.json"));
+
+         JSONObject jsonObject = (JSONObject) obj;
+         Map<String, Object> parameters = new HashMap<String, Object>();
+         parameters.put("key_name", "myKey");
+         parameters.put("image_id", "3b7be1fa-d381-4067-bb81-e835df630564");
+         parameters.put("instance_type", "SMALL_1");
+
+         CreateStackOptions createStackOptions = CreateStackOptions.Builder.template(String.valueOf(jsonObject.get("template"))).parameters(parameters);
+         String stackName = getName();
+         Stack stack = stackApi.create(stackName, createStackOptions);
+         assertThat(stack).isNotNull();
+         assertThat(stack.getId()).isNotEmpty();
+         FluentIterable<StackResource> resources = stackApi.listStackResources(stackName, stack.getId());
+         assertThat(resources).isNotNull();
+         assertThat(resources).isNotEmpty();
+         for (StackResource stackResource : resources) {
+            assertThat(stackResource).isNotNull();
+            String stackResourceName = stackResource.getName();
+            assertThat(stackResourceName).isNotNull();
+            assertThat(stackResourceName).isNotEmpty();
+            assertThat(stackResource.getStatus()).isNotEqualTo(StackResource.Status.UNRECOGNIZED);
+            StackResource resourceFromGet = stackApi.getStackResource(stackName, stack.getId(), stackResourceName);
+            assertThat(resourceFromGet).isNotNull();
+            assertThat(resourceFromGet.getName()).isEqualTo(stackResourceName);
+         }
+      }
+   }
+
+   public void testGetStackResourceMetadata() throws ParseException {
+      for (String region : api.getConfiguredRegions()) {
+         StackApi stackApi = api.getStackApi(region);
+         JSONParser parser = new JSONParser();
+         Object obj = parser.parse(stringFromResource("/simple_stack.json"));
+
+         JSONObject jsonObject = (JSONObject) obj;
+
+         CreateStackOptions createStackOptions = CreateStackOptions.Builder.template(String.valueOf(jsonObject.get("template")));
+         String stackName = getName();
+         Stack stack = stackApi.create(stackName, createStackOptions);
+         assertThat(stack).isNotNull();
+         assertThat(stack.getId()).isNotEmpty();
+         FluentIterable<StackResource> resources = stackApi.listStackResources(stackName, stack.getId());
+         assertThat(resources).isNotNull();
+         assertThat(resources).isNotEmpty();
+         for (StackResource stackResource : resources) {
+            assertThat(stackResource).isNotNull();
+            String stackResourceName = stackResource.getName();
+            assertThat(stackResourceName).isNotNull();
+            assertThat(stackResourceName).isNotEmpty();
+            assertThat(stackResource.getStatus()).isNotEqualTo(StackResource.Status.UNRECOGNIZED);
+            Map metadata = stackApi.getStackResourceMetadata(stackName, stack.getId(), stackResourceName);
+            assertThat(metadata).isNotNull();
+            assertThat(metadata).isNotEmpty();
+         }
       }
    }
 
